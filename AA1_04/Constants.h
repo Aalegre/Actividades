1ae7a53d9a034a434c9b8492cc537d220d4a0cd8
Alberto Alegre Mora <alberto.alegre@enti.cat>
2019-02-28 15:54:18 +0100

    AA1_04 Nuevo proyecto

---
+++ AA1_04/Constants.h
@@ -0,0 +1,3 @@
+#pragma once
+
+const int STONE_PERCENT = 20, COIN_PERCENT = 30;
---
+++ AA1_04/Types.h
@@ -0,0 +1,7 @@
+#pragma once
+
+enum Movement {
+	UP = 'w', DOWN = 's', LEFT = 'a', RIGHT = 'd', EXIT = 'x',
+	UP = 'W', DOWN = 'S', LEFT = 'A', RIGHT = 'D', EXIT = 'X'
+};
+enum Items { PATH = '.', STONE = '#', COIN = '$', PLAYER = '@' };
