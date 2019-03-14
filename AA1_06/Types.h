26f37d29f012302fa0c8a391eafb48dab24b8804
Alberto Alegre Mora <alberto.alegre@enti.cat>
2019-03-14 17:47:28 +0100

    AA1_06 v2

---
+++ AA1_06/Types.h
@@ -0,0 +1,37 @@
+#pragma once
+
+
+struct Date {
+	unsigned int day = 0;
+	unsigned int month = 0;
+	unsigned int year = 0;
+	Date() {}
+	Date(unsigned int day_, unsigned int month_, unsigned int year_) {
+		day = day_; month = month_; year = year_;
+	}
+	//std::string print() {
+	//	return std::to_string(day) + "/" + std::to_string(month) + "/" + std::to_string(year);
+	//}
+};
+
+char letrasDNI[] = { 'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E' };
+struct Persona {
+	std::string nombre = "";
+	std::string apellido1 = "";
+	std::string apellido2 = "";
+	unsigned int nDNI = 47988121;
+	char letraDNI = ' ';
+	Date nacimiento;
+
+
+	char getLetraDNI(unsigned int nDNI_) {
+		return letrasDNI[nDNI_ % 23];
+	}
+	Persona() {};
+	Persona(std::string nombre_, unsigned int nDNI_, Date nacimiento_) {
+		nombre = nombre_; nDNI = nDNI_; letraDNI = getLetraDNI(nDNI_); nacimiento = nacimiento_;
+	}
+	Persona(std::string nombre_, std::string apellido1_, std::string apellido2_, unsigned int nDNI_, Date nacimiento_) {
+		nombre = nombre_; apellido1 = apellido1_; apellido2 = apellido2_; nDNI = nDNI_; letraDNI = getLetraDNI(nDNI_); nacimiento = nacimiento_;
+	}
+};
--- AA1_06/VectorTest.cpp
+++ AA1_06/VectorTest.cpp
@@ -1,12 +1,56 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
+#include "Types.h"
+
+std::vector<int> addVectors(std::vector<int> v1_, std::vector<int> v2_) {
+	std::vector<int> result(v1_.size());
+	if (v1_.size() == v2_.size()) {
+		for (int i = 0; i < v1_.size(); i++)
+		{
+			result[i] = v1_[i] + v2_[i];
+		}
+	}
+	return result;
+}
+void concatVectors(std::vector<std::string> &v1_, std::vector<std::string> v2_) {
+	for (int i = 0; i < v2_.size(); i++)
+	{
+		v1_.push_back(v2_[i]);
+	}
+}
+
+void shiftRight(std::vector<int> &v_) {
+	v_.insert(v_.begin(), rand() % 101);
+}
+
+void rotateLeft() {
+
+}
+
+void searchAdjacent() {
+
+}
+
+bool isPalindrome(std::vector<char> &v_) {
+	std::vector<char> v1;
+	std::vector<char> v2;
+	return (v1 == v2);
+}
 
 void main() {
+	srand(time(NULL));
 	std::vector<int> v1(5);
 	std::vector<int> v2(10, 0);
 	std::vector<int> v3 = { 10,9,8,7,6,5,4,3,2,1 };
 	std::vector<std::string> v4 = { "Gertrudiz","Pancracia","Anacleto","Hipolito","Eustaquio","Fulgencia" };
 	std::vector<int> v5(v3);
-	std::vector<int> v6(v3);
-
+	std::vector<Persona> v6(3);
+	for (int i = 0; i < v6.size(); i++)
+	{
+		v6[i] = Persona(v4[rand() % v4.size()], rand() % 89999999 + 10000000, Date(rand() % 30 + 1, rand() % 11 + 1, rand() % 100 + 1919));
+	}
+	std::cout << "Introduzca un valor para salir...";
+	char exit;
+	std::cin >> exit;
 }
